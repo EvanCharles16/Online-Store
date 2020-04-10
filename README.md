acf0d9d2f7a79b57f5819485cf272b47fbbb785e
EvanCharles16 <evancharles1609@gmail.com>
2020-04-10 19:59:31 +0700

    responsive slider

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 
 Hover image
 https://miketricking.github.io/bootstrap-image-hover/
+
+Cart (1Jam)
+https://github.com/john-smilga/js-cart-setup
+
+Shopping Cart (4jam) img css
+https://github.com/john-smilga/setup-files-js-comfy-house
--- js/main.js
+++ js/main.js
@@ -10,13 +10,35 @@ $(".slider-one")
     });
 
 // Second Slider
-$(".slider-two")
-    .not(".slick-initalized")
-    .slick({
+    $('.slider-two').not(".slick-initalized").slick({
+        speed: 600,
         prevArrow:".site-slider-two .prev",
         nextArrow:".site-slider-two .next",
-        slidesToShow: 5,
-        slidesToScroll: 1,
-        autoplaySpeed: 3000
-
-    });
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 3,
+              infinite: true,
+              dots: true
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+        ]
+      });
